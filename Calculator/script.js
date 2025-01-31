1bd7194a75df29fca4a0782154faad12a7de0475
Danial <126600890+danial87j@users.noreply.github.com>
2025-01-31 17:57:27 +0330

    Create script.js

---
+++ Calculator/script.js
@@ -0,0 +1,14 @@
+// Put your input in num1 and num2 and calc should always be set to 0 so it can do the processes
+let calc = 0,
+  num1 = 5,
+  num2 = 2;
+calc = num1 + num2;
+console.log(`${num1} + ${num2} =`, calc);
+calc = num1 - num2;
+console.log(`${num1} - ${num2} =`, calc);
+calc = num1 * num2;
+console.log(`${num1} * ${num2} =`, calc);
+calc = num1 / num2;
+console.log(`${num1} / ${num2} =`, calc);
+calc = num1 % num2;
+console.log(`${num1} % ${num2} =`, calc);
