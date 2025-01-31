1fe8c21837aec508ee767f5bcde0008d58cd7249
Danial <126600890+danial87j@users.noreply.github.com>
2025-01-31 17:52:00 +0330

    Create code.js

---
+++ schoolProject/code.js
@@ -0,0 +1,90 @@
+let school = {
+  ClassA: {
+    Student1ClassA: {
+      FirstName: "danial",
+      LastName: "jalali",
+      Phone: "09364149023",
+      Address: "Shiraz",
+      Grade: 18.83,
+    },
+    Student2ClassA: {
+      FirstName: "Amirali",
+      LastName: "Chahbejari",
+      Phone: "09172386646",
+      Address: "Bushehr",
+      Grade: 17.33,
+    },
+    Student3ClassA: {
+      FirstName: "MohammadReza",
+      LastName: "IDK",
+      Phone: "IDK",
+      Address: "Bushehr",
+      Grade: 19.33,
+    },
+  },
+  ClassB: {
+    Student1ClassB: {
+
+        FirstName: "Farbod",
+        LastName: "Mohammadi",
+        Phone: "09173822134",
+        Address: "Tehran",
+        Grade: 19.93
+
+    },
+    Student2ClassB: {
+
+        FirstName: "Reza",
+        LastName: "Rajabi",
+        Phone: "09173237910",
+        Address: "Ilam",
+        Grade: 17.93
+
+    },
+    Student3ClassB: {
+
+        FirstName: "Parham",
+        LastName: "Parvini",
+        Phone: "09182379102",
+        Address: "Gilan",
+        Grade: 16.93
+
+    },
+  }
+};
+console.log("Student1ClassA :")
+console.log("Student1ClassA-FirstName:", school.ClassA.Student1ClassA.FirstName)
+console.log("Student1ClassA-LastName:", school.ClassA.Student1ClassA.LastName)
+console.log("Student1ClassA-PhoneNumber:", school.ClassA.Student1ClassA.Phone)
+console.log("Student1ClassA-Address:", school.ClassA.Student1ClassA.Address)
+console.log("Student1ClassA-Grade:", school.ClassA.Student1ClassA.Grade)
+console.log("Student2ClassA:")
+console.log("Student2ClassA-FirstName:", school.ClassA.Student2ClassA.FirstName)
+console.log("Student2ClassA-LastName:", school.ClassA.Student2ClassA.LastName)
+console.log("Student2ClassA-PhoneNumber:", school.ClassA.Student2ClassA.Phone)
+console.log("Student2ClassA-Address:", school.ClassA.Student2ClassA.Address)
+console.log("Student2ClassA-Grade:", school.ClassA.Student2ClassA.Grade)
+console.log("Student3ClassA :")
+console.log("Student3ClassA-FirstName:", school.ClassA.Student3ClassA.FirstName)
+console.log("Student3ClassA-LastName:", school.ClassA.Student3ClassA.LastName)
+console.log("Student3ClassA-PhoneNumber:", school.ClassA.Student3ClassA.Phone)
+console.log("Student3ClassA-Address:", school.ClassA.Student3ClassA.Address)
+console.log("Student3ClassA-Grade:", school.ClassA.Student3ClassA.Grade)
+console.log("Student1ClassB :")
+console.log("Student1ClassB-FirstName:", school.ClassB.Student1ClassB.FirstName)
+console.log("Student1ClassB-LastName:", school.ClassB.Student1ClassB.LastName)
+console.log("Student1ClassB-PhoneNumber:", school.ClassB.Student1ClassB.Phone)
+console.log("Student1ClassB-Address:", school.ClassB.Student1ClassB.Address)
+console.log("Student1ClassB-Grade:", school.ClassB.Student1ClassB.Grade)
+console.log("Student2ClassB :")
+console.log("Student2ClassB-FirstName:", school.ClassB.Student2ClassB.FirstName)
+console.log("Student2ClassB-LastName:", school.ClassB.Student2ClassB.LastName)
+console.log("Student2ClassB-PhoneNumber:", school.ClassB.Student2ClassB.Phone)
+console.log("Student2ClassB-Address:", school.ClassB.Student2ClassB.Address)
+console.log("Student2ClassB-Grade:", school.ClassB.Student2ClassB.Grade)
+console.log("Student3ClassB :")
+console.log("Student3ClassB-FirstName:", school.ClassB.Student3ClassB.FirstName)
+console.log("Student3ClassB-LastName:", school.ClassB.Student3ClassB.LastName)
+console.log("Student3ClassB-PhoneNumber:", school.ClassB.Student3ClassB.Phone)
+console.log("Student3ClassB-Address:", school.ClassB.Student3ClassB.Address)
+console.log("Student3ClassB-Grade:", school.ClassB.Student3ClassB.Grade)
